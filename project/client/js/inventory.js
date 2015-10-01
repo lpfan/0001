c250b51781872adabe6be84559bfe3b6d48e114d
Chybaka <vvvooova@gmail.com>
2015-10-01 22:58:59 +0300

    Плач і споглядай!)) мені самому сподобалося, майже - в коллекції тільки не впевнений, а так все працює практично ідеально))))

--- project/client/js/inventory.js
+++ project/client/js/inventory.js
@@ -1,19 +1,24 @@
 /**
  * Created by vova on 29.09.2015.
  */
+var customers = new Mongo.Collection("customers");
+var problems = [];
+Session.set("prob", "");
 Template.inventory.events({
+
     "click #cancel": function(){
         "use strict";
         location.assign("http://localhost:3000/index.html");
         location.reload(true);
     },
+
     "click .form": function(event){
         "use strict";
         if ($(event.target).css("background-color") === "rgb(255, 0, 0)"){
             $(event.target).css("background-color", "rgb(221, 221, 221)");
         }
         else{
-            $(event.target).css("background-color", "red");
+            $(event.target).css("background-color", "rgb(255, 0, 0)");
         }
     },
 
@@ -21,16 +26,9 @@ Template.inventory.events({
         "use strict";
         var problem = $("#elseInput").val().trim();
         if (problem) {
-
-
-            problems.push(problem);
-            Session.set("prob", problems);
-
-
+            Session.set("prob", problem);
         }
-
-            console.log(problem);
-
+        $("#elseInput").val("");
     },
 
     "click #add":function(){
@@ -40,30 +38,71 @@ Template.inventory.events({
         var phone =$("#phone").val().trim();
         var deadline = $("#deadline").val().trim();
         var price = $("#price").val().trim();
-        var newObject = bike+name+deadline;
-        /*
-
-        name: name,
+        var troubles = {};
+        $(".form").each(function(){
+            if ($(this).css("background-color") === "rgb(255, 0, 0)"){
+                troubles[$(this).text()] = false;
+            }
+        });
+        problems.forEach(function(trouble){
+            troubles[trouble] = false;
+        });
+        customers.insert({
+            bike: bike,
+            name: name,
             phone: phone,
             deadline: deadline,
-            price: price
-        customer.insert({
-
+            price: price,
+            troubles: troubles
         });
-        */
-        var troubles ={};
+        problems.splice(0,problems.length);
+        location.assign("http://localhost:3000/index.html");
+        location.reload(true);
+    },
 
-
-
+    "mousedown p": function(event){
+        "use strict";
+        var edit = $(event.target).text(), k;
+        if (event.which === 1){
+            var input = $("#elseInput");
+            problems.splice(problems.indexOf(edit), 1);
+            if (input.val().trim()){
+                var problem = input.val().trim();
+                Session.set("prob", problem);
+                input.val(edit);
+                input.focus();
+            }
+            else{
+                input.val(edit);
+                input.focus();
+                if (problems.length % 2 === 1){
+                    k = "controlTroublesNumber1";
+                }
+                else{
+                    k = "controlTroublesNumber2";
+                }
+                Session.set("prob", k);
+            }
+        }
+        else{
+            problems.splice(problems.indexOf(edit), 1);
+            if (problems.length % 2 === 1){
+                k = "controlTroublesNumber1";
+            }
+            else{
+                k = "controlTroublesNumber2";
+            }
+            Session.set("prob", k);
+        }
     }
 });
-//var problems = ["segsdg","egfsgfsadf"];
-//problems.push.Session.get("prob");
+
 Template.inventory.helpers({
     problem: function(){
-
-
-    return problems;}
-
-
+        var a = Session.get("prob");
+        if ((a) && (a !== "controlTroublesNumber1") && (a !== "controlTroublesNumber2") && (problems.indexOf(a) === -1)){
+            problems.push(a);
+        }
+    return problems;
+    }
 });
